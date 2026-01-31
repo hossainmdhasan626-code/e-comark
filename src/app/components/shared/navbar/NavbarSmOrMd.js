47693fa984b648bb18cae534f57ffb4a903a4011
Hasan <hasan@hasan.netis.cc>
2026-01-31 22:35:06 +0600

    add suspense to NavbarSmOrMd.js

--- src/app/components/shared/navbar/NavbarSmOrMd.js
+++ src/app/components/shared/navbar/NavbarSmOrMd.js
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import WrapperWithMainColor from "../../ui(reusable)/WrapperWithMainColor";
 import Search from "../header/Search";
 import Sidbar from "../header/Sidbar";
@@ -7,11 +7,11 @@ import { useSelector } from "react-redux";
 import SignInOut from "../header/SignInUp";
 import Link from "next/link";
 
-const NavbarSmOrMd = ({drawerItems}) => {
+const NavbarSmOrMd = ({ drawerItems }) => {
   const user = useSelector((state) => state.auth);
 
   return (
-    <div>
+    <Suspense fallback={<div>Loading Navigation...</div>}>
       {/* thisComponentRendardTheSearchBar */}
       <div className="md:hidden">
         {/*  thisWrapperWasJustAColorWrapperWichIsOnliForMdScreen */}
@@ -19,7 +19,7 @@ const NavbarSmOrMd = ({drawerItems}) => {
           {/* inThisWrapperWithMainColorComponentWasForMdScreen */}
           <div className="w-full flex justify-around items-center">
             <div className="text-white">
-              <Sidbar drawerItems={drawerItems}/>
+              <Sidbar drawerItems={drawerItems} />
             </div>
 
             <div>
@@ -29,24 +29,24 @@ const NavbarSmOrMd = ({drawerItems}) => {
             <div>
               {user?.fullName ? (
                 <Link href={"/profile"}>
-                <label
-                  htmlFor="my-drawer-5"
-                  className="drawer-button btn bg-mainColor border-mainColor hover:bg-[#1a1a1a]"
-                >
-                  {/* rendarTheProfileLogo */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="35"
-                    height="35"
-                    viewBox="0 0 24 24"
-                    className="text-white"
+                  <label
+                    htmlFor="my-drawer-5"
+                    className="drawer-button btn bg-mainColor border-mainColor hover:bg-[#1a1a1a]"
                   >
-                    <path
-                      fill="currentColor"
-                      d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2m-7 3c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6m7 13H5v-.23c0-.62.28-1.2.76-1.58C7.47 15.82 9.64 15 12 15s4.53.82 6.24 2.19c.48.38.76.97.76 1.58z"
-                    />
-                  </svg>
-                </label>
+                    {/* rendarTheProfileLogo */}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="35"
+                      height="35"
+                      viewBox="0 0 24 24"
+                      className="text-white"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2m-7 3c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6m7 13H5v-.23c0-.62.28-1.2.76-1.58C7.47 15.82 9.64 15 12 15s4.53.82 6.24 2.19c.48.38.76.97.76 1.58z"
+                      />
+                    </svg>
+                  </label>
                 </Link>
               ) : (
                 <SignInOut />
@@ -59,7 +59,7 @@ const NavbarSmOrMd = ({drawerItems}) => {
       <div className="hidden md:block">
         <Search />
       </div>
-    </div>
+    </Suspense>
   );
 };
 
