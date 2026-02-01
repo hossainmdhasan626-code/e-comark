995a76ff71b719415ef411cc0d36fee0dedcaa63
Hasan <hasan@hasan.netis.cc>
2026-02-01 18:56:53 +0600

    mobile screen er slidbar er w fix korlam

--- src/app/components/shared/header/Sidbar.js
+++ src/app/components/shared/header/Sidbar.js
@@ -86,7 +86,7 @@ const Sidbar = ({ drawerItems }) => {
               <li key={item?.id}>
                 <button
                   onClick={() => onItemClick(item)}
-                  className="btn bg-mainColor border-2 border-white text-white hover:bg-[#1a1a1a] transition-all w-5 flex justify-start items-center min-h-[3rem]"
+                  className="btn bg-mainColor border-2 border-white text-white hover:bg-[#1a1a1a] transition-all flex justify-start items-center min-h-[3rem] w-full"
                 >
                   {item?.name}
                 </button>
