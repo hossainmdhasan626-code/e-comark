39e397c40291dc568da89db1d36f01da74c9edfa
mainbsl4 <mainbsl4@gmail.com>
2026-06-21 02:02:12 +0600

    feat: implement sign-up form component and validation schema

--- src/app/components/shared/forms/schema/SignUpSchema.js
+++ src/app/components/shared/forms/schema/SignUpSchema.js
@@ -15,17 +15,17 @@ export const signUpSchema = Yup.object({
     .min(10, "Minimum 10 digits required")
     .required("Phone number is required"),
 
-  // password: Yup.string()
-  //   .required("Password is required")
-  //   .min(8, "Password must be at least 8 characters")
-  //   .matches(/[a-z]/, "At least one lowercase letter (a-z)")
-  //   .matches(/[A-Z]/, "At least one uppercase letter (A-Z)")
-  //   .matches(/[0-9]/, "At least one number (0-9)")
-  //   .matches(/[!@#$%^&*]/, "At least one special character (@, #, $)"),
+  password: Yup.string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters")
+    .matches(/[a-z]/, "At least one lowercase letter (a-z)")
+    .matches(/[A-Z]/, "At least one uppercase letter (A-Z)")
+    .matches(/[0-9]/, "At least one number (0-9)")
+    .matches(/[!@#$%^&*]/, "At least one special character (@, #, $)"),
 
-  // confirmPassword: Yup.string()
-  //   .oneOf([Yup.ref("password"), null], "Passwords must match")
-  //   .required("Confirm your password"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Passwords must match")
+    .required("Confirm your password"),
 
   terms: Yup.boolean()
     .oneOf([true], "You must accept the terms and conditions")
