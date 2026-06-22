bb71a906aea8a7d103671d424d2cbcc6d02a28eb
mainbsl4 <mainbsl4@gmail.com>
2026-06-23 03:07:29 +0600

    feat: add Address component with RTK Query integration and location validation schema

--- src/app/components/shared/forms/schema/profilePageSchemas/ProfileAddressSchema.js
+++ src/app/components/shared/forms/schema/profilePageSchemas/ProfileAddressSchema.js
@@ -2,14 +2,12 @@ import * as Yup from "yup";
 
 export const ProfileAddressSchema = Yup.object({
   alias: Yup.string(),
-  firstName: Yup.string().required("First name is required"),
-  lastName: Yup.string().required("Last name is required"),
-  company: Yup.string(),
   address: Yup.string().required("Address is required"),
   addressLine2: Yup.string(),
-  zipCode: Yup.string(),
+  zipCode: Yup.string().required("Zip/Postal code is required"),
   city: Yup.string().required("City is required"),
+  district: Yup.string().required("District is required"),
+  upazila: Yup.string().required("Upazila is required"),
   country: Yup.string().required("Country is required"),
   mobilePhone: Yup.string().required("Mobile phone is required"),
-  phone: Yup.string(),
 });
